b3753526b2f97bbebb414978e30f692528c5f495
hothamphong <hothamphong21031999@gmail.com>
2022-09-30 14:29:34 +0700

    #90

--- src/services/userService.js
+++ src/services/userService.js
@@ -71,6 +71,10 @@ const postVerifyBookAppointment = (data) => {
     return axios.post(`api/verify-book-appointment`, data)
 }
 
+const createNewSpecialty = (data) => {
+    return axios.post(`api/create-new-specialty`, data)
+}
+
 export {
     handleLoginApi, getAllUsers,
     createNewUserService, deleteUserService,
@@ -79,5 +83,6 @@ export {
     saveDetailDoctorService, getDetailInforDoctor,
     saveBulkScheduleDoctor, getScheduleDoctorByDate,
     getExtraInforDoctorById, getProfileDoctorById,
-    postPatientBookAppointment, postVerifyBookAppointment
+    postPatientBookAppointment, postVerifyBookAppointment,
+    createNewSpecialty
 }
